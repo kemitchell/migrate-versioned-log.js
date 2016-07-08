16206f9ac8b9ba58c0f2caff04fc11ff356d5905
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-07 23:45:38 -0700

    Refactor and comment for readability

--- index.js
+++ index.js
@@ -5,31 +5,22 @@ var semver = require('semver')
 var through2 = require('through2')
 
 module.exports = function (transforms) {
-  transforms.forEach(function (transform) {
-    var badRange = !semver.gtr(transform.toVersion, transform.fromRange)
-    if (badRange) {
-      throw new Error('toVersion must be greater than fromRange')
-    }
-    var missingTransform = (
-      !transform.hasOwnProperty('transform') ||
-      typeof transform.transform !== 'function'
-    )
-    if (missingTransform) throw new Error('missing transform function')
-  })
+  transforms.forEach(validateTransform)
 
   return through2.obj(function (chunk, encoding, done) {
     var self = this
-    if (!('entry' in chunk)) done(new Error('no entry'))
-    else if (!('version' in chunk)) done(new Error('no version'))
-    else {
-      migrate(chunk, function (error, newChunks) {
-        if (error) done(error)
-        else {
-          newChunks.forEach(function (chunk) { self.push(chunk) })
-          done()
-        }
-      })
-    }
+    validateChunk(chunk, function (error) {
+      if (error) done(error)
+      else {
+        migrate(chunk, function (error, migratedChunks) {
+          if (error) done(error)
+          else {
+            migratedChunks.forEach(function (chunk) { self.push(chunk) })
+            done()
+          }
+        })
+      }
+    })
   })
 
   function migrate (chunk, callback) {
@@ -40,19 +31,22 @@ module.exports = function (transforms) {
     })
     if (transform === undefined) callback(null, [chunk])
     else {
-      transform.transform(chunk.entry, function (error, newEntries) {
+      transform.transform(chunk.entry, function (error, migratedEntries) {
         if (error) callback(error)
         else {
-          var newChunks = newEntries.map(function (entry) {
+          var migratedChunks = migratedEntries.map(function (migratedEntry) {
             return {
+              // "Inherit" the index number of the migrated entry.
               index: chunk.index,
+              // Use the version of the transform.
               version: transform.toVersion,
-              entry: entry
+              entry: migratedEntry
             }
           })
-          asyncMap(newChunks, migrate, function (error, newChunks) {
+          // Recurse.
+          asyncMap(migratedChunks, migrate, function (error, recursed) {
             if (error) callback(error)
-            else callback(null, flatten(newChunks))
+            else callback(null, flatten(recursed))
           })
         }
       })
@@ -60,8 +54,26 @@ module.exports = function (transforms) {
   }
 }
 
-function flatten (array) {
-  return array.reduce(function (flattened, element) {
+function validateTransform (transform) {
+  var badRange = !semver.gtr(transform.toVersion, transform.fromRange)
+  if (badRange) {
+    throw new Error('toVersion must be greater than fromRange')
+  }
+  var missingTransform = (
+    !transform.hasOwnProperty('transform') ||
+    typeof transform.transform !== 'function'
+  )
+  if (missingTransform) throw new Error('missing transform function')
+}
+
+function validateChunk (chunk, callback) {
+  if (!('entry' in chunk)) callback(new Error('no entry'))
+  else if (!('version' in chunk)) callback(new Error('no version'))
+  else callback()
+}
+
+function flatten (arrayOfArrays) {
+  return arrayOfArrays.reduce(function (flattened, element) {
     return flattened.concat(element)
   }, [])
 }
